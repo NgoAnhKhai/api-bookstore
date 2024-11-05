77431d4aa068b5ff47453ca22f7f09861260c825
NgoAnhKhai <anhkhaichannel@gmail.com>
2024-11-05 20:49:31 +0700

    code

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+const cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+
+var app = express("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+// Bắt các yêu cầu không khớp với bất kỳ route nào
+app.use((req, res, next) => {
+  const exception = new Error("Path not found");
+  exception.statusCode = 404;
+  next(exception); // Chuyển lỗi đến middleware tiếp theo
+});
+app.use((err, req, res, next) => {
+  res.status(err.statusCode).send(err.message);
+});
+module.exports = app;
---
+++ routes/book.api.js
@@ -0,0 +1,190 @@
+const fs = require("fs");
+const crypto = require("crypto");
+const express = require("express");
+const router = express.Router();
+
+//get
+router.get("/", (req, res, next) => {
+  // input validation (nếu có)
+  const allowedFilter = [
+    "author",
+    "country",
+    "language",
+    "title",
+    "page",
+    "limit",
+  ];
+
+  try {
+    let { page, limit, ...filterQuery } = req.query;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 10;
+    const filterKeys = Object.keys(filterQuery);
+    filterKeys.forEach((key) => {
+      if (!allowedFilter.includes(key)) {
+        const exception = new Error(`Query ${key} is not allowed`);
+        exception.statusCode = 401;
+        throw exception;
+      }
+      if (!filterQuery[key]) delete filterQuery[key];
+    });
+  } catch (error) {
+    next(error);
+  }
+
+  let offset = limit * (page - 1);
+
+  let db = fs.readFileSync("db.json", "utf-8");
+
+  db = JSON.parse(db);
+
+  const { books } = db;
+
+  let result = [];
+
+  if (filterKeys.length) {
+    filterKeys.forEach((condition) => {
+      result = result.length
+        ? result.filter((book) => book[condition] === filterQuery[conditon])
+        : books.filter((book) => book[condition] === filterQuery[condition]);
+    });
+  } else {
+    result = books;
+  }
+  console.log(result);
+
+  result = result.slice(offset, offset + limit);
+
+  res.status(200).send(result);
+});
+
+// post
+router.post("/", (req, res, next) => {
+  try {
+    const { author, country, imageLink, language, pages, title, year } =
+      req.body;
+    if (
+      !author ||
+      !country ||
+      !imageLink ||
+      !language ||
+      !pages ||
+      !title ||
+      !year
+    ) {
+      const exception = new Error(`missing body info`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+
+    const newBook = {
+      author,
+      country,
+      imageLink,
+      language,
+      pages: parseInt(pages) || 1,
+      title,
+      year: parseInt(year) || 0,
+      id: crypto.randomBytes(4).toString("hex"),
+    };
+
+    // Read data from db.json then parse to JSObject
+    let db = fs.readFileSync("db.json", "utf-8");
+    db = JSON.parse(db);
+    const { books } = db;
+
+    // add new book to book JS object
+    books.push(newBook);
+    db.books = books;
+
+    // convert db JSObject to JSON string
+    db = JSON.stringify(db);
+
+    // write and save to db.json
+    fs.writeFileSync("db.json", db);
+
+    // send response after successfully writing to db.json
+    res.status(200).send(newBook);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// put
+router.put("/:bookId", (req, res, next) => {
+  const allowUpdate = [
+    "author",
+    "country",
+    "imageLink",
+    "language",
+    "pages",
+    "title",
+    "year",
+  ];
+  const { bookId } = req.params;
+  const updates = req.body;
+  const updateKeys = Object.keys(updates);
+
+  try {
+    const notAllow = updateKeys.filter((el) => !allowUpdate.includes(el));
+    if (notAllow.length) {
+      const exception = new Error(`update field not allow`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+
+    let db = fs.readFileSync("db.json", "utf-8");
+    db = JSON.parse(db);
+    const { books } = db;
+
+    const targetIndex = books.findIndex((book) => book.id === bookId);
+    if (targetIndex < 0) {
+      const exception = new Error(`Book not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+
+    const updatedBook = { ...books[targetIndex], ...updates };
+    books[targetIndex] = updatedBook;
+
+    db.books = books;
+    db = JSON.stringify(db);
+
+    fs.writeFileSync("db.json", db);
+
+    // send response
+    res.status(200).send(updatedBook);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//delete
+router.delete("/:bookId", (req, res, next) => {
+  //delete input validation
+  try {
+    const { bookId } = req.params;
+  } catch (error) {
+    next(error);
+  }
+
+  //delete processing logic
+  let db = fs.readFileSync("db.json", "utf-8");
+  db = JSON.parse(db);
+  const { books } = db;
+  //find book by id
+  const targetIndex = books.findIndex((book) => book.id === bookId);
+  if (targetIndex < 0) {
+    const exception = new Error(`book not found`);
+    exception.statusCode = 404;
+    throw exception;
+  }
+  //filter db books obj
+  db.books = books.filter((book) => book.id !== bookId);
+  db = JSON.stringify(db);
+  fs.writeFileSync("db.json", db);
+  //delete send response
+  res.status(200).send({});
+});
+
+module.exports = router;
